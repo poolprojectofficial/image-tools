c1f36b3f85cc8298507a90d309eb94872b30c22f
Muniz <rodrigotmuniz@gmail.com>
2023-04-01 11:59:20 -0300

    adding gravity positions

--- src/composite/index.ts
+++ src/composite/index.ts
@@ -3,26 +3,59 @@ import path from 'path'
 import sharp from 'sharp'
 
 const dirPath = '/home/muniz/Downloads/image-small'
+const side = 128
+const dirName = `${side}-${side}-black`
 const dirImagesExtension = '.jpg'
-const blackImageFileName = '128x128-black.jpg'
-const blackImageFileNameWithoutExtension = blackImageFileName.split('.')[0]
+let blackBgImage: Buffer
+const gravityElements = ['north','northeast','east','southeast','south','southwest','west','northwest','center','centre',]
 
 export const composite = async () => {
-  const images = fs.readdirSync(dirPath, { encoding: 'utf-8' }).filter((a) => a.includes(dirImagesExtension))
-  const resultDirExists = fs.existsSync(path.join(dirPath, blackImageFileName.split('.')[0]))
-
-  if (!resultDirExists) fs.mkdirSync(path.join(dirPath, blackImageFileNameWithoutExtension))
+  await updateBlackBgImageVar()
+  const images = await getImageNames()
+  await createDirIfNotExisted()
 
   for (const image of images) {
     await createNewCompositeImage(image)
   }
 }
 
+const createDirIfNotExisted = async () => {
+  const dirPathAndName = path.join(dirPath, dirName)
+  const dirExists = fs.existsSync(dirPathAndName)
+  if (!dirExists) fs.mkdirSync(dirPathAndName)
+}
+
+const getImageNames = async () => {
+  return fs.readdirSync(dirPath, { encoding: 'utf-8' }).filter((a) => a.includes(dirImagesExtension))
+}
+
+const updateBlackBgImageVar = async (): Promise<void> => {
+  blackBgImage = await sharp({
+    create: {
+      width: side,
+      height: side,
+      channels: 3,
+      background: { r: 0, g: 0, b: 0 }
+    }
+  })
+    .png({ quality: 100 })
+    .toBuffer()
+}
+
 const createNewCompositeImage = async (imageName: string) => {
+    const randomIndex = Math.round(Math.random() * 10) - 1
+
   try {
-    await sharp(path.join('src/composite', blackImageFileName))
-      .composite([{ input: path.join(dirPath, imageName), blend: 'over' }])
-      .toFile(path.join(dirPath, blackImageFileNameWithoutExtension, `${imageName}`))
+    await sharp(blackBgImage)
+      .composite([
+        {
+          input: path.join(dirPath, imageName),
+          gravity: gravityElements[randomIndex] 
+        }
+      ])
+      // .withMetadata()
+      .png({ quality: 100 })
+      .toFile(path.join(dirPath, dirName, `${imageName}`))
   } catch (err) {
     console.log(imageName, err)
   }
